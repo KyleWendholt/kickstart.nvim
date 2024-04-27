0978e4108b3d5d7d3d0b66b23424a7fe3f2178bf
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2024-04-27 01:32:48 -0400

    switch to regular rose-pine

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -2,7 +2,7 @@ return {
   'rose-pine/neovim',
   as = 'rose-pine',
   config = function()
-    vim.cmd 'colorscheme rose-pine-moon'
+    vim.cmd 'colorscheme rose-pine'
     -- Set background
     -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
