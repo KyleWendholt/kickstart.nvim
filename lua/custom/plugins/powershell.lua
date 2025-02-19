6175de414bd62651729514978fd082461fc36124
Kyle Wendholt <wendholk1@newpaltz.edu>
2025-02-18 22:23:20 -0500

    update powershell config to use pwsh 7 and be os agnostic

--- lua/custom/plugins/powershell.lua
+++ lua/custom/plugins/powershell.lua
@@ -2,7 +2,7 @@ return {
   'TheLeoP/powershell.nvim',
   ---@type powershell.user_config
   opts = {
-    bundle_path = 'C:/Users/wendholk1/AppData/Local/nvim-data/mason/packages/powershell-editor-services',
-    shell = 'powershell.exe',
+    -- bundle_path = 'C:/Users/wendholk1/AppData/Local/nvim-data/mason/packages/powershell-editor-services',
+    bundle_path = vim.fn.stdpath 'data' .. '/mason/packages/powershell-editor-services',
   },
 }
