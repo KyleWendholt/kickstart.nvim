6621e96dc0fedd76e89ddeced7020643f9924f93
Kyle Wendholt <wendholk1@newpaltz.edu>
2025-02-18 21:27:00 -0500

    update colors and add powershell config

---
+++ lua/custom/plugins/powershell.lua
@@ -0,0 +1,8 @@
+return {
+  'TheLeoP/powershell.nvim',
+  ---@type powershell.user_config
+  opts = {
+    bundle_path = 'C:/Users/wendholk1/AppData/Local/nvim-data/mason/packages/powershell-editor-services',
+    shell = 'powershell.exe',
+  },
+}
